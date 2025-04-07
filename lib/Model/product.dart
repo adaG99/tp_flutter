575cb85fc337a3fc2d6eeaff137818fd4cdf0290
adaG99 <bebead14@gmail.com>
2025-04-07 16:39:03 +0200

    third commit

--- lib/Model/product.dart
+++ lib/Model/product.dart
@@ -2,14 +2,12 @@ class Product {
   final int id;
   final String title;
   final double price;
-  final String description;
   final String image;
 
   Product({
     required this.id,
     required this.title,
     required this.price,
-    required this.description,
     required this.image,
   });
 
@@ -18,7 +16,6 @@ class Product {
       id: json['id'],
       title: json['title'],
       price: (json['price'] as num).toDouble(),
-      description: json['description'],
       image: json['image'],
     );
   }
